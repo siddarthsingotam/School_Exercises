496be8f7d6649108b3d9bc4aed182815ae0e415b
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-08-26 13:47:33 +0530

    Old to New weight sys

--- Exercise_2/2.5.py
+++ Exercise_2/2.5.py
@@ -6,8 +6,8 @@ pou = float(input("Enter Pounds:"))
 lot = float(input("Enter Lots :"))
 
 grams = (tal*20*32*13.3) + (pou*32*13.3) + (lot*13.3)
-kilo = (grams / 1000)
-
-print("the weight is:", kilo, "in Kg")
-
-
+kilr = (grams / 1000)
+kilo = (grams // 1000)
+gramz = ( kilr - kilo)
+print("the weight is:")
+print( kilo,"kilo grams and", (round(gramz, 3)), "grams" )
