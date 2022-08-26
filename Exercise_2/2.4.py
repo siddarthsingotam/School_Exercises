258b57c3ae46ecd88abdbd637ed81bd62c15cd24
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-08-26 12:18:58 +0530

    Average of Numbers

---
+++ Exercise_2/2.4.py
@@ -0,0 +1,12 @@
+print("Type 3 numbers")
+num1 = int(input("Enter no. 1 :"))
+num2 = int(input("Enter no. 2 :"))
+num3 = int(input("Enter no. 3: "))
+
+adn = num1+num2+num3
+pro = num1*num2*num3
+avg = adn/3
+
+print("The sum of 3 numbers is :",adn)
+print("the product of 3 numbers is :",pro)
+print("The average of the numbers is :",avg)
