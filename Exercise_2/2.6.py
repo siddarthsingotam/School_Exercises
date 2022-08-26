05ee50c1f1ce0addc96e70e5063a557532006eeb
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-08-26 14:34:42 +0530

    Random codes for locks

---
+++ Exercise_2/2.6.py
@@ -0,0 +1,9 @@
+import random
+#I googled for random combinations and learned about import function
+rand3 = random.sample (range(0,9),3)
+rand4 = random.sample(range(0,9),4)
+
+print("Possible Codes are for lock are listed below")
+print("3 digit-random-code :", rand3)
+print("4 digit-random-code :", rand4)
+
