48f3883c0aca0aa348d67f66fefbe871f717a0a4
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-09-01 14:05:19 +0530

    Guess the number (GAME)

---
+++ Exercise_4/4.4.py
@@ -0,0 +1,19 @@
+import random
+number = random.randint(1,10)
+guess_taken = 0
+
+print("Welcome to the guessing game!\nGuess a number between 1 and 10")
+while guess_taken != number :
+    guess = int(input("Take a guess :"))
+    guess_taken = guess_taken + 1
+
+    if guess < number:
+        print("Your guess is low")
+
+    if guess > number:
+        print("Your guess is high")
+
+    if guess == number:
+        print("your guess is correct")
+
+
