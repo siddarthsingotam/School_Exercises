775ffaad8d9161e36aebed07f86bd9cff8a7f4ce
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-09-01 14:15:03 +0530

    Guess the number (GAME)

--- Exercise_4/4.4.py
+++ Exercise_4/4.4.py
@@ -16,5 +16,6 @@ while guess_taken != number :
     if guess == number:
         print("your guess is correct")
         print("yay the number is", number, "you took", guess_taken,"guesse(s)")
+        #change formatted
 
 
