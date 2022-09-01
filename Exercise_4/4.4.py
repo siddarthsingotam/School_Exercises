f8b97221eef28ba6708b3c193bf82b2a332a2458
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-09-01 23:30:07 +0530

    Guessing Game

--- Exercise_4/4.4.py
+++ Exercise_4/4.4.py
@@ -3,7 +3,7 @@ number = random.randint(1,10)
 guess_taken = 0
 
 print("Welcome to the guessing game!\nGuess a number between 1 and 10")
-while guess_taken != number :
+while guess_taken != "":
     guess = int(input("Take a guess :"))
     guess_taken = guess_taken + 1
 
@@ -15,7 +15,7 @@ while guess_taken != number :
 
     if guess == number:
         print("your guess is correct")
-        print("yay the number is", number, "you took", guess_taken,"guesse(s)")
+        print("yay the number is", number, "you took", guess_taken,"guess(es)")
         #change formatted
 
 
