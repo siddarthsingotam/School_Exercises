7c00d3013005b94fc1be789a73872574056bd895
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-09-09 13:15:12 +0530

    Zander basket

--- Exercise_3/3.1.py
+++ Exercise_3/3.1.py
@@ -1,9 +1,9 @@
 import math
-print ("Zander Calculator")
+print("Zander Calculator")
 length = float(input("Enter the lenth of the Zander in (cm) :"))
 
 if length >= 42:
     print("Add to basket!")
 else:
-    print("Zander to be release back into the water")
+    print("Zander to be release back into the water. It is ", 42 - length, "cm smaller than limit length")
 
