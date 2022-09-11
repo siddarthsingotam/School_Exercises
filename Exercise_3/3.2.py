748f8e10054d44185c7136c1e8b3f03efd521862
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-09-11 18:16:49 +0530

    Choose your voyage #Made improvements

--- Exercise_3/3.2.py
+++ Exercise_3/3.2.py
@@ -2,15 +2,15 @@ print('''Welcome aboard!
 You can choose your travel experience via choosing our classes available on our ship. The classes include :''')
 print("1.LUX \n" "2.A \n""3.B \n""3.C \n")
 print("For more details enter cabin class below.")
-cabin_class = str(input("Enter cabin class : "))
+cabin_class = input("Enter cabin class : ").upper()
 
-if cabin_class == str("LUX") or str("lux") :
+if cabin_class == "LUX":
     print("upper-deck cabin with a balcony.")
-elif cabin_class == str("A") or str("a") :
+elif cabin_class == "A":
     print("above the car deck, equipped with a window.")
-elif cabin_class == str("B") or str("b"):
+elif cabin_class == "B":
     print("windowless cabin above the car deck.")
-elif cabin_class == str("C") or str("c") :
+elif cabin_class == "C":
     print("windowless cabin below the car deck.")
 else:
-    print("INVALID CABIN CLASS!")
+    print(f"INVALID CABIN CLASS!")
