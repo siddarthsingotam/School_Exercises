c989ef4068a177dd76f669e38186dd9aedb70f14
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-09-12 09:56:23 +0530

    Make your own dice by giving the no. of sides

---
+++ Exercise_6/6.2.py
@@ -0,0 +1,14 @@
+# let sides be the parameter for defining the number of sides in the cube
+def dice(sides):
+    import random
+
+    x = random.randint(1, sides)
+    rolls = 0
+    while x != sides:
+        x = random.randint(1, sides)
+        rolls = rolls + 1
+        print(f"For roll {rolls} we get side {x}")
+    print(f"Therefore it took {rolls} rolls to get side {sides}")
+
+
+dice(21)
