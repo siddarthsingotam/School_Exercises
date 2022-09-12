f827becd0c438a3d8fca21febaff6dd6a321bd39
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-09-12 11:59:44 +0530

    Function using a list #Here we use addition of list items to test the function.

---
+++ Exercise_6/6.4.py
@@ -0,0 +1,7 @@
+list_1 = [1, 2, 3, 4, 5]
+
+def function_list(list_1):
+    add = sum(list_1)
+    return add
+#formula is defined, printing formula...
+print(f"The sum of the numbers in the list is: {function_list(list_1)}")
