1129c5b93edb586a305d47f2c351ddccd1d4fa1f
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-09-12 09:31:08 +0530

    Dice output per each roll

---
+++ Exercise_6/6.1.py
@@ -0,0 +1,11 @@
+def dice():
+    import random
+    x = random.randint(1, 6)
+    rolls = 0
+    while x != 6:
+        x = random.randint(1, 6)
+        rolls = rolls + 1
+        print(f"For roll {rolls} we get output {x}")
+    print(f"Therefore it took {rolls} rolls to get 6")
+
+dice()
