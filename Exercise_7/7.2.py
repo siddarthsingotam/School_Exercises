9a19ca024caf7b92f465eedfd03284acb2088f3f
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-09-23 07:12:24 +0530

    Name set

--- Exercise_7/7.2.py
+++ Exercise_7/7.2.py
@@ -1,7 +1,7 @@
 # Making an empty set
 name_set = set()
 print(f"\nThis is a new data structure, enter names to fill. \nPress enter to quit the loop.")
-name_add = input(f"\nEnter the first name: ").capitalize()
+name_add = input(f"\nEnter the first name: ").capitalize()# Improved code by capitalizing the first letter of the name
 print("new name")
 # While loop helps to read each name and tell us if the name exists or not.
 while name_add != "":
@@ -17,3 +17,4 @@ while name_add != "":
 print(f"\nThe names in the set are: \n")
 for names in name_set:
     print(names.capitalize())
+
