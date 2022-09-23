05496b811049d821b8f7c21b1949f524e2450877
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-09-23 07:09:21 +0530

    Area of Rectangle

--- Exercise_7/7.2.py
+++ Exercise_7/7.2.py
@@ -1,12 +1,12 @@
 # Making an empty set
 name_set = set()
 print(f"\nThis is a new data structure, enter names to fill. \nPress enter to quit the loop.")
-name_add = input(f"\nEnter the first new name: ")
-
+name_add = input(f"\nEnter the first name: ").capitalize()
+print("new name")
 # While loop helps to read each name and tell us if the name exists or not.
 while name_add != "":
     name_set.add(name_add)
-    name_add = input("Enter name again: ")
+    name_add = input("Enter name again: ").capitalize()
     if name_add not in name_set:
         print("new name")
     if name_add in name_set:
