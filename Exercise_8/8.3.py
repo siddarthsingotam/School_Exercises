396f36bd1c494f0df05b1459d9e2863f2461a9e9
siddarthsingotam <siddarthsingotam1213@gmail.com>
2022-09-29 00:00:25 +0530

    Distance betwwen two airports

---
+++ Exercise_8/8.3.py
@@ -0,0 +1,47 @@
+import mysql.connector
+from geopy import distance
+
+
+def AirportCoordinates(icao_code):
+    sql = 'SELECT latitude_deg, longitude_deg FROM airport WHERE gps_code = ' + f'"{icao_code}"' + ";"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+
+
+connection = mysql.connector.connect(
+    host="127.0.0.1",
+    port="3306",
+    database="flight_game",
+    user="root",
+    password="yoyo@123",
+    autocommit=True
+
+)
+
+# Asking input
+icao_code_1 = input("Enter Nation Code of Airport 1: ")
+
+
+icao_code_2 = input("Enter Nation Code of Airport 2: ")
+
+coordinates_1 = []
+
+coordinates_2 = []
+
+# For-loop
+
+for i in AirportCoordinates(icao_code_1)[0]:
+    coordinates_1.append(float(i))
+
+for i in AirportCoordinates(icao_code_2)[0]:
+    coordinates_2.append(float(i))
+
+# Converting list to tuple
+
+coordinates_1 = tuple(coordinates_1)
+coordinates_2 = tuple(coordinates_2)
+
+print(f"The distance between Area 1: {coordinates_1} and Area 2: {coordinates_2} is {distance.distance(coordinates_1, coordinates_2 )}")
+print("Bon voyage!")
